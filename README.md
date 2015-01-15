28c7985a4c9cb17fab41a4acea0dff5c72383904
kaareltonisson <kaareltonisson@gmail.com>
2015-01-15 14:08:20 +0200

    Eigenvector finding moved to main, eigenvectors are now written to eigenvector_results for debugging / comparing to MATLAB.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ Parallel2014
 ============
 
 Parallel Computing project
+
+Face database: http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
--- code/src/main.f90
+++ code/src/main.f90
@@ -1,23 +1,32 @@
     program main
     implicit none
     integer iheight, iwidth, Maxwidth, Maxheight, i, j
-    integer :: classes, training, N, class_train
+    integer :: classes, training, N, LDA, LDVL, LDVR, LWMAX, class_train
 	character(len=250) :: imgfolder
     parameter( classes=10, training=9 )
     parameter(class_train = classes*training)
-    parameter(Maxwidth=3220,Maxheight=2415)    
-	parameter	(N=5)
-	parameter ( imgfolder = 'C:\Users\Antti\GitHub\Palallel2014\code\pictures3' ) 
+    parameter(Maxwidth=3220,Maxheight=2415)
+	parameter(N=5)
+	parameter(LDA=class_train,LDVL=class_train,LDVR=class_train, LWMAX = 1000)
+	parameter ( imgfolder = 'C:\Users\Kaarel\Documents\Paralleelarvutused\projekt\code\pictures3' ) 
     integer image(Maxheight,Maxwidth)  
     double precision :: mean(N), matmean(N, N)
-    !double precision :: mat(N,N) = (/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /)
-    double precision :: mat(N,N) = (/ -1.01, 3.98, 3.30, 4.43, 7.31, 0.86, 0.53, 8.26, 4.96,-6.43, -4.60,-7.04,-3.89,-7.66,-6.16, 3.31, 5.29, 8.20,-7.33, 2.47, -4.81, 3.55,-1.51, 6.18, 5.58 /)
-    real(kind=8) :: trainimg(92*112, classes*training), testimg(92*112, classes*(10-training)), meanimg(92*112), norm_img(92*112, classes*training), norm_test(92*112, classes*(10-training))
+    double precision :: trainimg(92*112, classes*training), testimg(92*112, classes*(10-training)), meanimg(92*112), norm_img(92*112, classes*training), norm_test(92*112, classes*(10-training))
     !DOUBLE PRECISION :: VR(N, N), VL(N, N), WR(N, N), WI(N, N)
 	double precision :: VR(class_train, class_train), VL(class_train, class_train), WR(class_train, class_train), WI(class_train, class_train)
     double precision :: Y(92*112, class_train), eig_temp(class_train, class_train), train_vec(class_train, class_train), test_vec(class_train, classes*(10-training))
     logical :: istraining
     
+	INTEGER :: LWORK,INFO
+	REAL :: WORK(5*class_train)
+	
+	! -- eigenvector testing --
+	double precision :: testWR(5,5), testWI(5,5), testVL(5,5), testVR(5,5),testWORK(20,20)
+	!double precision :: mat(N,N) = (/ 1, 2, 3, 4, 5, 6, 7, 8, 9 /)
+    double precision :: mat(5,5) = (/ -1.01, 3.98, 3.30, 4.43, 7.31, 0.86, 0.53, 8.26, 4.96,-6.43, -4.60,-7.04,-3.89,-7.66,-6.16, 3.31, 5.29, 8.20,-7.33, 2.47, -4.81, 3.55,-1.51, 6.18, 5.58 /)
+	!! CALL DGEEV( 'N', 'V', 5, mat, 5, testWR, testWI, testVL, 5, testVR, 5, testWORK, 20, INFO )
+	!! CALL PRINT_EIGENVECTORS( 'Test Right eigenvectors', 5, testWI, testVR, 5 )
+	
     double precision :: min_dist(classes*(10-training)), new_dist
     integer :: min_index(classes*(10-training))
     integer recog;
@@ -56,15 +65,26 @@
 	
     eig_temp = MATMUL(TRANSPOSE(norm_img), norm_img)
     !print*, 'norm_img', norm_img
-    !print*, 'eig_temp', eig_temp
-    
-	call RIGHTEIGENVECTOR(eig_temp, N, N, VR, VL, N, WR, WI)
+    print*, 'eig_temp', SIZE(eig_temp, 1),SIZE(eig_temp, 2)
+	print*,'class_train', class_train
+	
+	! -- calculate eigenvector WORK size -- !! broken, gives 0
+	LWORK = -1
+	call DGEEV( 'N', 'V', class_train, eig_temp, LDA, WR, WI, VL, LDVL, VR, LDVR, WORK, LWORK, INFO )
+	print*, 'work', MIN( LWMAX, INT( WORK( 1 ) ) ) 
+	print*, 'info', INFO
+	! LWORK = MIN( LWMAX, INT( WORK( 1 ) ) )  !! gives 0, not usable?
+	LWORK = 200 * class_train
+	
+	! -- find eigenvectors --
+	call DGEEV( 'N', 'V', class_train, eig_temp, LDA, WR, WI, VL, LDVL, VR, LDVR, WORK, LWORK, INFO )
+
 	!print *, 'Eigenvectors:', char(10), VR	    
-    
+    CALL WRITE_EIGENVECTORS( 'Right eigenvectors', class_train, WI, VR, class_train )
     !CALL PRINT_EIGENVECTORS( 'Left eigenvectors', N, WI, VL, N )    
-    !CALL PRINT_EIGENVECTORS( 'Right eigenvectors', class_train, WI, VR, class_train )
+	!CALL PRINT_EIGENVECTORS( 'Right eigenvectors', class_train, WI, VR, class_train )
         
-    !Calculate training vector
+    ! -- Calculate training vector --
     Y = MATMUL(norm_img, VR)
     !Reduce dimensionality
     train_vec = MATMUL(TRANSPOSE(Y), norm_img)
